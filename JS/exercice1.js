d516b7b962e942f27c1057d66d00b75e5f3f5221
achille56 <leclercqjm59@gmail.com>
2023-01-03 10:31:21 +0100

    exo 111

---
+++ JS/exercice1.js
@@ -0,0 +1,9 @@
+let math = $('#math-heading');
+    console.log(math);
+
+math.html(math.html() + ' ...ouahou !');
+
+let sciences = $('#science-heading');
+    console.log(sciences);
+
+sciences.html(sciences.html() + ' ...amazing !!!');
